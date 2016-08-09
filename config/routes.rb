8da13f0fb9a5ffbce2417f9b7d7cbe5a02931167
Andrea Cavallo <A.Cavallo1@newcastle.ac.uk>
2016-08-09 14:32:56 +0100

    exo1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'tasks/index'
+
+  # get 'tasks/show'
+
+  # get 'tasks/new'
+
+  # get 'tasks/create'
+
+  # get 'tasks/edit'
+
+  # get 'tasks/update'
+
+  # get 'tasks/destroy'
+
+
+ get '/tasks', to: 'tasks#index'
+
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+
+  post 'tasks', to: 'tasks#create'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch "tasks/:id" => "tasks#update"
+
+  delete "tasks/:id" => "tasks#destroy"
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+
+  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
